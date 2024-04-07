9bf5296eb94009e741557c55c27faa26d121c8ec
coldapollo8 <63075497+coldapollo8@users.noreply.github.com>
2024-04-07 18:30:30 -0400

    submission

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/MergerRepository.java
@@ -0,0 +1,20 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.repository;
+
+import edu.iu.habahram.DinerPancakeHouseMerge.model.CafeMenu;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.DinerMenu;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Menu;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.PancakeHouseMenu;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class MergerRepository {
+    public ArrayList<Menu> getTheMenus() {
+        ArrayList<Menu> menus = new ArrayList<>();
+        menus.add(new DinerMenu());
+        menus.add(new PancakeHouseMenu());
+        menus.add(new CafeMenu());
+        return menus;
+    }
+}
